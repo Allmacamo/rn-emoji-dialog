854b7a08fbe24ab30ded863b593ffcf8e733c98d
Furkan Bozdag <bozd4g@hotmail.com>
2019-08-17 23:11:25 +0300

    Add view component

---
+++ src/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+class EmojiDialog extends React.Component {
+   render() {
+      return (
+         <View>
+            <Text>Hello World</Text>
+         </View>
+      );
+   }
+}
+
+export default EmojiDialog;
