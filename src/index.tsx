781d02f91b3c8a97aae35d1a54f967a53a11734c
Furkan Bozdag <bozd4g@hotmail.com>
2019-09-01 12:16:22 +0300

    Complete first version of this component

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { View, Text, Animated, Alert } from 'react-native';
+import { View, Text, Animated, Alert, TextStyle, TouchableWithoutFeedback } from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
 import { IButton, IEmoji } from '../models';
 import styles from './styles';
@@ -10,56 +10,122 @@ interface IEmojiDialogProps {
    title?: string;
    message?: string;
    size?: number;
-   buttons?: IButton[];
+   button?: IButton;
    visible: boolean;
+   onBackgroundClick?(): void;
 }
 
-interface IEmojiDialogState {}
+interface IEmojiDialogState {
+   fadingValue: Animated.Value;
+   visible: boolean;
+}
 
 class EmojiDialog extends React.Component<IEmojiDialogProps, IEmojiDialogState> {
    constructor(props: IEmojiDialogProps) {
       super(props);
+
+      this.state = {
+         fadingValue: new Animated.Value(0),
+         visible: false,
+      };
    }
 
    getButtons = () => {
-      return this.props.buttons ? (
-         <View style={styles.buttons}>
-            {this.props.buttons.map((button: IButton) => (
-               <Text key={button.id} style={[styles.button]} onPress={button.onClick!}>
-                  {button.text}
-               </Text>
-            ))}
+      const buttonsContainer = this.props.type === 'primary' ? styles.primaryContainer : styles.secondaryContainer;
+      const buttonStyle = this.props.type === 'primary' ? styles.primaryButton : [styles.secondaryButton];
+      if (this.props.type === 'secondary') (buttonStyle as any).button = { color: this.props.emoji.colors[0], fontWeight: 'bold' } as TextStyle;
+
+      return this.props.button ? (
+         <View style={[styles.buttons, buttonsContainer]}>
+            <TouchableWithoutFeedback onPress={this.props.button!.onClick}>
+               <View style={[styles.button, buttonStyle]}>
+                  <Text key={this.props.button!.id} style={(buttonStyle as any).button} onPress={this.props.button!.onClick}>
+                     {this.props.button!.text}
+                  </Text>
+               </View>
+            </TouchableWithoutFeedback>
          </View>
       ) : (
          undefined
       );
    };
 
-   render() {
-      const { type, emoji, title, message, size, visible } = this.props;
-      const emojiSize = size ? size : 65;
+   /***
+    * TODO:
+    * This function will be change with getDerivedStateFromProps
+    * when React releases the new major version is 17.x.x
+    */
+   UNSAFE_componentWillReceiveProps(nextProps: IEmojiDialogProps) {
+      if (nextProps.visible) {
+         this.setState({ visible: true });
+      }
 
-      return (
-         <React.Fragment>
-            <Animated.View style={{}}>
-               <View style={[styles.container, { display: visible ? 'flex' : 'none' }]}>
-                  <View style={styles.containerContent}>
-                     <View style={[styles.emoji, { top: (emojiSize / 2) * -1 }]}>
-                        <React.Suspense fallback={<View />}>
-                           <emoji.Component height={emojiSize} width={emojiSize} />
-                        </React.Suspense>
-                     </View>
-                     <LinearGradient start={{ x: 0.0, y: 1.0 }} end={{ x: 1.0, y: 0.0 }} colors={emoji.colors} style={[styles.gradientBg]}>
-                        <View style={[styles.content]}>
-                           <Text style={styles.title}>{title}</Text>
-                           <Text style={styles.message}>{message}</Text>
-                        </View>
-                     </LinearGradient>
-                  </View>
-               </View>
-            </Animated.View>
-         </React.Fragment>
+      Animated.timing(this.state.fadingValue, {
+         toValue: nextProps.visible ? 1 : 0,
+         duration: 300,
+      }).start(() =>
+         this.setState({
+            visible: nextProps.visible,
+         }),
       );
+   }
+
+   render() {
+      const { type, emoji, title, message, size, button, visible, onBackgroundClick } = this.props;
+
+      const emojiSize = size ? size : 65;
+      const gradientStyle = button ? (type === 'primary' ? { paddingVertical: 15 } : { paddingBottom: 50 }) : { paddingBottom: 20 };
+
+      return this.state.visible ? (
+         <React.Fragment>
+            <TouchableWithoutFeedback onPress={onBackgroundClick}>
+               <Animated.View
+                  style={[
+                     {
+                        opacity: this.state.fadingValue.interpolate({
+                           inputRange: [0, 1],
+                           outputRange: [0, 1],
+                        }),
+                        transform: [
+                           {
+                              scale: this.state.fadingValue.interpolate({
+                                 inputRange: [0, 1],
+                                 outputRange: [1, 1],
+                              }),
+                           },
+                        ],
+                     },
+                     styles.container,
+                  ]}
+               >
+                  <TouchableWithoutFeedback>
+                     <View style={styles.containerContent}>
+                        <View style={[styles.emoji, { top: (emojiSize / 2) * -1 }]}>
+                           <React.Suspense fallback={<View />}>
+                              <emoji.Component height={emojiSize} width={emojiSize} />
+                           </React.Suspense>
+                        </View>
+                        <LinearGradient
+                           start={{ x: 0.0, y: 1.0 }}
+                           end={{ x: 1.0, y: 0.0 }}
+                           colors={emoji.colors}
+                           style={[styles.gradientBg, gradientStyle]}
+                        >
+                           <View style={[styles.content]}>
+                              <Text style={styles.title}>{title}</Text>
+                              <Text style={styles.message}>{message}</Text>
+                           </View>
+
+                           {type === 'primary' ? this.getButtons() : undefined}
+                        </LinearGradient>
+
+                        {type === 'secondary' ? this.getButtons() : undefined}
+                     </View>
+                  </TouchableWithoutFeedback>
+               </Animated.View>
+            </TouchableWithoutFeedback>
+         </React.Fragment>
+      ) : null;
    }
 }
 
--- src/styles.ts
+++ src/styles.ts
@@ -1,4 +1,4 @@
-import { ViewStyle, Dimensions, TextStyle } from 'react-native';
+import { ViewStyle, Dimensions, TextStyle, View } from 'react-native';
 
 const deviceHeight = Dimensions.get('screen').height;
 const deviceWidth = Dimensions.get('screen').width;
@@ -36,11 +36,10 @@ export default {
    gradientBg: {
       position: 'relative',
       borderRadius: borderRadius,
-      paddingHorizontal: '5%',
+      paddingHorizontal: '7.5%',
       flexDirection: 'column',
       justifyContent: 'space-between',
       paddingTop: 40,
-      paddingBottom: 20,
    } as ViewStyle,
    content: {
       justifyContent: 'center',
@@ -48,11 +47,20 @@ export default {
    title: {
       color: 'white',
       fontSize: 23,
+      fontWeight: 'bold',
    } as TextStyle,
    message: {
       color: 'rgba(255,255,255, .85)',
       fontSize: 14,
    } as TextStyle,
+   primaryContainer: {
+      marginTop: 10,
+   } as ViewStyle,
+   secondaryContainer: {
+      position: 'absolute',
+      justifyContent: 'center',
+      bottom: -15,
+   } as ViewStyle,
    buttons: {
       display: 'flex',
       flexDirection: 'row',
@@ -60,7 +68,19 @@ export default {
       width: '100%',
    } as ViewStyle,
    button: {
-      fontSize: 16,
-      marginHorizontal: 5,
+      marginHorizontal: 10,
+   } as TextStyle,
+   primaryButton: {
+      button: {
+         color: 'white',
+         fontWeight: 'bold',
+      } as TextStyle,
+   } as TextStyle,
+   secondaryButton: {
+      backgroundColor: 'white',
+      paddingVertical: 10,
+      paddingHorizontal: 25,
+      borderRadius: 25,
+      button: {} as TextStyle,
    } as TextStyle,
 };
