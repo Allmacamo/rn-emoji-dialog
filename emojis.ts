1b9a2833429ba7413943328b1e5bcedb9fe5de86
Furkan Bozdag <bozd4g@hotmail.com>
2019-09-03 21:33:21 +0300

    Add new 19 emojis

--- emojis.ts
+++ emojis.ts
@@ -1,15 +1,158 @@
 import { IEmoji } from './models';
 
+const grinning = {
+   name: 'grinning',
+   colors: ['#00c6ff', '#0072ff'],
+   content: require('./assets/png/1f600.png'),
+} as IEmoji;
+
+const grinningWithSmilingEyes = {
+   name: 'grinningWithSmilingEyes',
+   colors: ['#00c6ff', '#0072ff'],
+   content: require('./assets/png/1f601.png'),
+} as IEmoji;
+
+const faceWithTearsOfJoy = {
+   name: 'faceWithTearsOfJoy',
+   colors: ['#00c6ff', '#0072ff'],
+   content: require('./assets/png/1f602.png'),
+} as IEmoji;
+
+const grinningWithBigEyes = {
+   name: 'grinningWithBigEyes',
+   colors: ['#00c6ff', '#0072ff'],
+   content: require('./assets/png/1f603.png'),
+} as IEmoji;
+
+const grinningFaceWithSmilingEyes = {
+   name: 'grinningFaceWithSmilingEyes',
+   colors: ['#00c6ff', '#0072ff'],
+   content: require('./assets/png/1f604.png'),
+} as IEmoji;
+
+const grinningFaceWithSweat = {
+   name: 'grinningFaceWithSweat',
+   colors: ['#00c6ff', '#0072ff'],
+   content: require('./assets/png/1f605.png'),
+} as IEmoji;
+
+const grinningSquintingFace = {
+   name: 'grinningSquintingFace',
+   colors: ['#00c6ff', '#0072ff'],
+   content: require('./assets/png/1f606.png'),
+} as IEmoji;
+
+const smilingFaceWithHalo = {
+   name: 'smilingFaceWithHalo',
+   colors: ['#00c6ff', '#0072ff'],
+   content: require('./assets/png/1f607.png'),
+} as IEmoji;
+
+const smilingFaceWithHorns = {
+   name: 'smilingFaceWithHorns',
+   colors: ['#43C6AC', '#F8FFAE'],
+   content: require('./assets/png/1f608.png'),
+} as IEmoji;
+
+const winkingFace = {
+   name: 'winkingFace',
+   colors: ['#00c6ff', '#0072ff'],
+   content: require('./assets/png/1f609.png'),
+} as IEmoji;
+
+const neutralFace = {
+   name: 'neutralFace',
+   colors: ['#00c6ff', '#0072ff'],
+   content: require('./assets/png/1f610.png'),
+} as IEmoji;
+
+const expressionlessFace = {
+   name: 'expressionlessFace',
+   colors: ['#00c6ff', '#0072ff'],
+   content: require('./assets/png/1f611.png'),
+} as IEmoji;
+
+const unamusedFace = {
+   name: 'unamusedFace',
+   colors: ['#00c6ff', '#0072ff'],
+   content: require('./assets/png/1f612.png'),
+} as IEmoji;
+
+const downcastFaceWithSweat = {
+   name: 'downcastFaceWithSweat',
+   colors: ['#00c6ff', '#0072ff'],
+   content: require('./assets/png/1f613.png'),
+} as IEmoji;
+
+const pensiveFace = {
+   name: 'pensiveFace',
+   colors: ['#00c6ff', '#0072ff'],
+   content: require('./assets/png/1f614.png'),
+} as IEmoji;
+
+const confusedFace = {
+   name: 'confusedFace',
+   colors: ['#00c6ff', '#0072ff'],
+   content: require('./assets/png/1f615.png'),
+} as IEmoji;
+
+const confoundedFace = {
+   name: 'confoundedFace',
+   colors: ['#00c6ff', '#0072ff'],
+   content: require('./assets/png/1f616.png'),
+} as IEmoji;
+
+const kissingFace = {
+   name: 'kissingFace',
+   colors: ['#00c6ff', '#0072ff'],
+   content: require('./assets/png/1f617.png'),
+} as IEmoji;
+
+const faceBlowingAKiss = {
+   name: 'faceBlowingAKiss',
+   colors: ['#00c6ff', '#0072ff'],
+   content: require('./assets/png/1f618.png'),
+} as IEmoji;
+
+const kissingFaceWithSmilingEyes = {
+   name: 'kissingFaceWithSmilingEyes',
+   colors: ['#00c6ff', '#0072ff'],
+   content: require('./assets/png/1f619.png'),
+} as IEmoji;
+
+const angryFace = {
+   name: 'angryFace',
+   colors: ['#00c6ff', '#0072ff'],
+   content: require('./assets/png/1f620.png'),
+} as IEmoji;
+
 const pouting = {
    name: 'pouting',
    colors: ['#f2709c', '#ff9472'],
    content: require('./assets/png/1f621.png'),
 } as IEmoji;
 
-const grinning = {
-   name: 'pouting',
-   colors: ['#00c6ff', '#0072ff'],
-   content: require('./assets/png/1f600.png'),
-} as IEmoji;
-
-export { pouting, grinning };
+export {
+   grinning,
+   grinningWithSmilingEyes,
+   faceWithTearsOfJoy,
+   grinningWithBigEyes,
+   grinningFaceWithSmilingEyes,
+   grinningFaceWithSweat,
+   grinningSquintingFace,
+   smilingFaceWithHalo,
+   smilingFaceWithHorns,
+   winkingFace,
+   neutralFace,
+   expressionlessFace,
+   unamusedFace,
+   downcastFaceWithSweat,
+   pensiveFace,
+   confusedFace,
+   confoundedFace,
+   kissingFace,
+   faceBlowingAKiss,
+   kissingFaceWithSmilingEyes,
+   angryFace,
+   pouting,
+};
